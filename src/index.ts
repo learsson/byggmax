9817255815344b989e162dbf98ee50f27299795c
Lars Larsson <lars.larsson@consid.se>
2023-01-11 15:21:59 +0100

    added fileoutput

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import { PuppeteerExtra } from 'puppeteer-extra';
 import * as puppeteer from 'puppeteer';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { appendFileSync } from 'fs';
+import path from 'path';
 
 const urls: string[] = [
   'https://www.byggmax.se/regel-95x95-p08195095',
   'https://www.byggmax.se/28x120-trall-impregnerad-gr%C3%B6n-p08728120',
 ];
+
+const writeLog = (productTitle?: string, price?: string) => {
+  const fileName = 'byggmax_priser.csv';
+  const date = new Date().toISOString();
+  const data = `${date};${productTitle};${price}\n`;
+  appendFileSync(path.resolve(__dirname, fileName), data, {
+    encoding: 'utf-8',
+  });
+};
 
 const scrape = async (url: string) => {
   const p = new PuppeteerExtra(puppeteer);
@@ -26,10 +37,11 @@ const scrape = async (url: string) => {
     const priceEl = document.querySelector('#maincontent *> .price');
     const priceOreEl = document.querySelector('#maincontent *> .cents-label');
 
-    return priceEl?.innerHTML + ',' + priceOreEl?.innerHTML;
+    return `${priceEl?.innerHTML}.${priceOreEl?.innerHTML}`;
   });
 
-  console.log(`${title}: ${price}`);
+  writeLog(title, price);
+  console.log(`${title}: ${price}.-`);
   page.close();
   browser.close();
 };
