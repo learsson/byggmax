66f04227107db31e44ec91f7cd12ad4caa862f69
Lars Larsson <lars.larsson@consid.se>
2023-01-11 14:02:29 +0100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,23 @@ const urls: string[] = [
 const scrape = async (url: string) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.setUserAgent(
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36',
+  );
+
   await page.goto(url);
 
   const title = await page.evaluate(
     () => document.querySelector('title')?.innerHTML,
   );
-  const priceKr = await page.evaluate(() =>
-    document
-      .querySelector('#maincontent *> .price')
-      ?.getAttribute('textContent'),
-  );
-  const priceOre = await page.evaluate(() =>
-    document
-      .querySelector('#maincontent *>  .cents-label')
-      ?.getAttribute('textContent'),
-  );
-  console.log(
-    `${title?.toString()}: ${priceKr?.toString()},${priceOre?.toString()}`,
-  );
+  const price = await page.evaluate(() => {
+    const priceEl = document.querySelector('#maincontent *> .price');
+    const priceOreEl = priceEl?.querySelector('.cents-label');
+
+    return priceEl?.innerHTML + ',' + priceOreEl?.innerHTML;
+  });
+
+  console.log(`${title}: ${price}`);
   page.close();
   browser.close();
 };
